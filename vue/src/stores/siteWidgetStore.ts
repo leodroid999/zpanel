f6a7c3297ad52f25f0f929c3aed1418dd3d17b9b
leodroid999 <jackyacoub047@gmail.com>
2023-10-13 06:14:03 +0900

    Sites widget on dashboard

---
+++ vue/src/stores/siteWidgetStore.ts
@@ -0,0 +1,210 @@
+import { defineStore, mapActions } from "pinia";
+const SERVER = "http://z-panel.io"
+
+export const useSiteWidgetStore= defineStore({
+  id: "siteWidgetStore",
+  state: () => {
+    return {
+        sites:null,
+        shortlinks:null,
+        shortlinkDomains:null
+    }
+  },
+  actions:{
+    async getWidgetSites(){
+      let options:any={
+        credentials: 'include'
+      }
+      try{
+        let response=await fetch(SERVER+`/portal/sitesWidget.php`,options);
+        if(response.ok){
+          let responseData=await response.json()
+          if(responseData.status=="ok"){
+            this.sites=responseData.sites
+          }
+          return responseData;
+        }
+        else{
+          if(response.status==401){
+            this.authenticated=false;
+            return {
+              error:"NOT_AUTHENTICATED",
+              message:"Your session expired, login again"
+            }
+          }
+          return {
+            error:"SERVER_ERROR",
+            message:"There was a error loading your info , try again later"
+          }
+        }
+      }
+      catch(err){
+        console.error(err);
+        return {
+          error:"SERVER_ERROR",
+          message:"There was a error processing your request , try again later"
+        }
+      }
+    },
+    async getShortlinkDomains(){
+      let options:any={
+        credentials: 'include'
+      }
+      try{
+        let response=await fetch(SERVER+`/portal/shortlinkDomains.php`,options);
+        if(response.ok){
+          let responseData=await response.json()
+          if(responseData.status=="ok"){
+            this.shortlinkDomains=responseData.shortlinkDomains
+          }
+          return responseData;
+        }
+        else{
+          if(response.status==401){
+            this.authenticated=false;
+            return {
+              error:"NOT_AUTHENTICATED",
+              message:"Your session expired, login again"
+            }
+          }
+          return {
+            error:"SERVER_ERROR",
+            message:"There was a error loading your info , try again later"
+          }
+        }
+      }
+      catch(err){
+        console.error(err);
+        return {
+          error:"SERVER_ERROR",
+          message:"There was a error processing your request , try again later"
+        }
+      }
+    },
+    async getShortlinks(){
+      let options:any={
+        credentials: 'include'
+      }
+      try{
+        let response=await fetch(SERVER+`/portal/shortlinks.php`,options);
+        if(response.ok){
+          let responseData=await response.json()
+          if(responseData.status=="ok"){
+            this.shortlinks=responseData.shortlinks
+          }
+          return responseData;
+        }
+        else{
+          if(response.status==401){
+            this.authenticated=false;
+            return {
+              error:"NOT_AUTHENTICATED",
+              message:"Your session expired, login again"
+            }
+          }
+          return {
+            error:"SERVER_ERROR",
+            message:"There was a error loading your info , try again later"
+          }
+        }
+      }
+      catch(err){
+        console.error(err);
+        return {
+          error:"SERVER_ERROR",
+          message:"There was a error processing your request , try again later"
+        }
+      }
+    },
+    async addShortLink(domain,destination,shortname){
+      let data=new FormData();
+      data.append('domain',domain)
+      data.append('destination',destination)
+      data.append('shortname',shortname)
+      let options={
+        method:"POST",
+        body: data,
+        credentials: 'include'
+      }
+      try{
+        let response=await fetch(SERVER+'/portal/addShortLink.php',options);
+        if(response.ok){
+          let responseData=await response.json()
+          return responseData;
+        }
+        else{
+          return {
+            error:"SERVER_ERROR",
+            message:"There was a error processing your request , try again later"
+          }
+        }
+      }
+      catch(err){
+        console.error(err);
+        return {
+          error:"SERVER_ERROR",
+          message:"There was a error processing your request , try again later"
+        }
+      }
+    },
+    async deleteShortLink(shortname){
+      let data=new FormData();
+      data.append('shortname',shortname)
+      let options={
+        method:"POST",
+        body: data,
+        credentials: 'include'
+      }
+      try{
+        let response=await fetch(SERVER+'/portal/removeShortLink.php',options);
+        if(response.ok){
+          let responseData=await response.json()
+          return responseData;
+        }
+        else{
+          return {
+            error:"SERVER_ERROR",
+            message:"There was a error processing your request , try again later"
+          }
+        }
+      }
+      catch(err){
+        console.error(err);
+        return {
+          error:"SERVER_ERROR",
+          message:"There was a error processing your request , try again later"
+        }
+      }
+    },
+    async editShortLink(shortname,destination){
+      let data=new FormData();
+      data.append('shortname',shortname)
+      data.append('destination',destination)
+      let options={
+        method:"POST",
+        body: data,
+        credentials: 'include'
+      }
+      try{
+        let response=await fetch(SERVER+'/portal/editShortLink.php',options);
+        if(response.ok){
+          let responseData=await response.json()
+          return responseData;
+        }
+        else{
+          return {
+            error:"SERVER_ERROR",
+            message:"There was a error processing your request , try again later"
+          }
+        }
+      }
+      catch(err){
+        console.error(err);
+        return {
+          error:"SERVER_ERROR",
+          message:"There was a error processing your request , try again later"
+        }
+      }
+    },
+  },
+});
--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -8,11 +8,13 @@ import 'jsvectormap/dist/maps/world.js';
 import 'jsvectormap/dist/css/jsvectormap.min.css';
 import { ref } from 'vue'
 import { useUserStore } from '@/stores/userStore';
+import { useSiteWidgetStore } from '@/stores/siteWidgetStore';
 
 const appVariable = useAppVariableStore();
 const sessionStore = useSessionStore();
 const statStore = useStatStore();
 const userStore = useUserStore();
+const siteWidgetStore = useSiteWidgetStore();
 
 export default {
 	components: {
@@ -21,15 +23,15 @@ export default {
 	data() {
 		return {
 			renderComponent: true,
-			osChartKey : 0,
+			osChartKey: 0,
 			chartCfg: {
-				"osChart":{
+				"osChart": {
 					height: 70,
 					options: {
-						chart: { 
+						chart: {
 							id: 'osChart',
-							type: 'donut', 
-							sparkline: { enabled: true } 
+							type: 'donut',
+							sparkline: { enabled: true }
 						},
 						theme: {
 							monochrome: {
@@ -39,46 +41,51 @@ export default {
 								shadeIntensity: 0.65
 							}
 						},
-						stroke: { 
-							show: false, 
-							curve: 'smooth', 
-							lineCap: 'butt', 
-							colors: 'rgba(' + appVariable.color.blackRgb + ', .25)', 
-							width: 2, dashArray: 0 
-						}, 
-						plotOptions: { 
-							pie: { 
-								donut: { background: 'transparent' } 
-							} 
-						} 
+						stroke: {
+							show: false,
+							curve: 'smooth',
+							lineCap: 'butt',
+							colors: 'rgba(' + appVariable.color.blackRgb + ', .25)',
+							width: 2, dashArray: 0
+						},
+						plotOptions: {
+							pie: {
+								donut: { background: 'transparent' }
+							}
+						}
 					},
 				}
 			}
 		}
 	},
 	methods: {
-		getOsData(){
-			if(statStore.stats && statStore.stats.os){
+		getOsData() {
+			if (statStore.stats && statStore.stats.os) {
 				return statStore.stats.os.map(os => os.Count)
 			}
 			return []
 		},
 		getMarkersData() {
-			let results=[]
-			if(statStore.stats && statStore.stats.coords){
-				for(let coord of statStore.stats.coords){
-					results.push({name:"",coords:[coord.lat,coord.lon]})
+			let results = []
+			if (statStore.stats && statStore.stats.coords) {
+				for (let coord of statStore.stats.coords) {
+					results.push({ name: "", coords: [coord.lat, coord.lon] })
 				}
 			}
 			return results;
 		},
-		getSelectedCountries(){
-			if(statStore.stats && statStore.stats.locations){
-				let countries=statStore.stats.locations.map(loc => loc.Country)
+		getSelectedCountries() {
+			if (statStore.stats && statStore.stats.locations) {
+				let countries = statStore.stats.locations.map(loc => loc.Country)
 				return [...new Set(countries)]
 			}
 			return []
 		},
+
+		loadSiteWidgetList() {
+			siteWidgetStore.getWidgetSites();
+		},
+
 		renderMap() {
 			document.getElementById('map-container').innerHTML = '<div id="map"></div>';
 			var map = new jsVectorMap({
@@ -102,24 +109,25 @@ export default {
 					}
 				},
 				regionStyle: {
-					 initial: { 
+					initial: {
 						fill: 'rgba(255, 255, 255, 0.75)', fillOpacity: 0.35, stroke: 'none', strokeWidth: 0.4, strokeOpacity: 1
-					 },
-				 	hover: { fillOpacity: 0.5 },
-					selected: { 
+					},
+					hover: { fillOpacity: 0.5 },
+					selected: {
 						fill: appVariable.color.theme, fillOpacity: 0.6, stroke: 'none', strokeWidth: 0.4, strokeOpacity: 1
-					 },
+					},
 				},
 				backgroundColor: 'transparent',
 				selectedRegions: this.getSelectedCountries()
 			});
 		},
-		onPanelChanged(){
-			let panel=sessionStore.selectedPanel;
-			statStore.getStats(panel.panelId,panel.nodeID);
+		onPanelChanged() {
+			let panel = sessionStore.selectedPanel;
+			statStore.getStats(panel.panelId, panel.nodeID);
 		},
-		timeAgo:function(str){;
-			const date= new Date(str+"Z").getTime()
+		timeAgo: function (str) {
+			;
+			const date = new Date(str + "Z").getTime()
 			const seconds = Math.floor(new Date().getTime() - date) / 1000;
 
 			let interval = Math.floor(seconds / 31536000);
@@ -147,85 +155,89 @@ export default {
 				return interval + ' minutes ago';
 			}
 
-			if(seconds < 10) return 'just now';
+			if (seconds < 10) return 'just now';
 
 			return Math.floor(seconds) + ' seconds ago';
 		},
-		calcPercent(location){
-			try{
-				let total = statStore.stats.locations.reduce((a,b)=>a+b.Count,0)
-				let percent=(location.Count*100/total).toFixed(2)
+		calcPercent(location) {
+			try {
+				let total = statStore.stats.locations.reduce((a, b) => a + b.Count, 0)
+				let percent = (location.Count * 100 / total).toFixed(2)
 				return percent
 			}
-			catch{
+			catch {
 				return "?";
 			}
 		},
-		renderOSChart(){
-			this.osChartKey +1;
+		renderOSChart() {
+			this.osChartKey + 1;
 		},
-		onPanelSelect(ev){
-			let selectedValue=ev.target.value;
-			if(selectedValue==""){
-				sessionStore.sessions=[]
+		onPanelSelect(ev) {
+			let selectedValue = ev.target.value;
+			if (selectedValue == "") {
+				sessionStore.sessions = []
 				return
 			}
-			let valueParts=selectedValue.split("@");
-			let panelId=valueParts[0];
-			let nodeId=valueParts[1];
-			sessionStore.selectedPanel=sessionStore.panels.find((panel)=>panel.panelId==panelId && panel.nodeID == nodeId) 
+			let valueParts = selectedValue.split("@");
+			let panelId = valueParts[0];
+			let nodeId = valueParts[1];
+			sessionStore.selectedPanel = sessionStore.panels.find((panel) => panel.panelId == panelId && panel.nodeID == nodeId)
 			sessionStore.selectedPanelName = selectedValue;
 			this.onPanelChanged();
 		}
 	},
-	computed:{
-		panels: function(){
+	computed: {
+		panels: function () {
 			return sessionStore.panels
 		},
-		stats: function(){
+		stats: function () {
 			return statStore.stats
 		},
-		user: function(){
+		user: function () {
 			return userStore.user
 		},
-		balance:function(){
+		balance: function () {
 			const userStore = useUserStore()
-			if(userStore.user && (userStore.user.balance !== null)){
-				let cents=parseFloat(userStore.user.balance)
-				let usd=(cents/100).toFixed(2);
-				let str=`${usd}\$`;
+			if (userStore.user && (userStore.user.balance !== null)) {
+				let cents = parseFloat(userStore.user.balance)
+				let usd = (cents / 100).toFixed(2);
+				let str = `${usd}\$`;
 				return str;
 			}
 			return "-"
 		},
-		selectedPanelName: function(){
+		selectedPanelName: function () {
 			return sessionStore.selectedPanelName
+		},
+		sitesWidget: function () {
+			return siteWidgetStore.sites
 		}
 	},
 	mounted() {
 		sessionStore.getPanelList(false);
-		sessionStore.$subscribe((mutation)=>{
+		sessionStore.$subscribe((mutation) => {
 			let ev = mutation.events;
-			if(ev && ev.key=="selectedPanel" && ev.oldValue != ev.newValue){
+			if (ev && ev.key == "selectedPanel" && ev.oldValue != ev.newValue) {
 				this.onPanelChanged()
 			}
 		})
-		statStore.$subscribe((mutation)=>{
+		statStore.$subscribe((mutation) => {
 			this.renderMap();
 			this.renderOSChart();
 		})
+		this.loadSiteWidgetList()
 	},
 	created() {
 		this.emitter.on('theme-reload', (evt) => {
 			this.renderComponent = false;
-			
+
 			this.$nextTick(() => {
 				this.renderComponent = true;
 				setTimeout(() => {
 					this.renderMap();
 				}, 50);
 			});
-    	})
+		})
 
 		window.addEventListener("resize", (event) => {
 			this.$nextTick(() => {
@@ -238,8 +250,8 @@ export default {
 }
 </script>
 <style>
-.panel-select{
-	background: rgba(0,0,0,0.3);
+.panel-select {
+	background: rgba(0, 0, 0, 0.3);
 	color: white;
 	padding: 0.5em;
 	border-radius: 0.5em;
@@ -247,136 +259,136 @@ export default {
 	margin-bottom: 0.5em;
 }
 
-.dashtable{
-	font-size:12.5px;
-    margin-bottom: 0rem;
+.dashtable {
+	font-size: 12.5px;
+	margin-bottom: 0rem;
 }
 
 
-.dashtable > :not(caption) > * > * {
-    padding: 0.25rem 0.15rem;
+.dashtable> :not(caption)>*>* {
+	padding: 0.25rem 0.15rem;
 }
 
 .online {
-  width: 15px;
-  height: 15px;
-  border-radius: 50%;
-  background-color: lightgreen;
-  white-space: nowrap; display: inline-block; vertical-align: middle;
-  margin-right:10px;
-  
+	width: 15px;
+	height: 15px;
+	border-radius: 50%;
+	background-color: lightgreen;
+	white-space: nowrap;
+	display: inline-block;
+	vertical-align: middle;
+	margin-right: 10px;
+
 }
 
 .warning {
-  width: 15px;
-  height: 15px;
-  border-radius: 50%;
-  background-color: gold;
-  white-space: nowrap; display: inline-block; vertical-align: middle;
-  margin-right:10px;  
+	width: 15px;
+	height: 15px;
+	border-radius: 50%;
+	background-color: gold;
+	white-space: nowrap;
+	display: inline-block;
+	vertical-align: middle;
+	margin-right: 10px;
 }
 
 
 .offline {
-  width: 15px;
-  height: 15px;
-  border-radius: 50%;
-  background-color: rgb(191, 69, 69);
-  white-space: nowrap; display: inline-block; vertical-align: middle;
-  margin-right:10px;
-  
+	width: 15px;
+	height: 15px;
+	border-radius: 50%;
+	background-color: rgb(191, 69, 69);
+	white-space: nowrap;
+	display: inline-block;
+	vertical-align: middle;
+	margin-right: 10px;
+
 }
 
 .warning {
-  width: 15px;
-  height: 15px;
-  border-radius: 50%;
-  background-color: rgb(218, 218, 0);
-  white-space: nowrap; display: inline-block; vertical-align: middle;
-  margin-right:10px;
-  
+	width: 15px;
+	height: 15px;
+	border-radius: 50%;
+	background-color: rgb(218, 218, 0);
+	white-space: nowrap;
+	display: inline-block;
+	vertical-align: middle;
+	margin-right: 10px;
+
 }
 </style>
 <template>
 	<select class="panel-select" @change="onPanelSelect($event)" v-model="selectedPanelName" :value="selectedPanelName">
-			<option v-if="panels" v-for="panel in panels" :value="panel.panelId+'@'+panel.nodeID">{{panel.panelId}} @ {{panel.nodeID }}</option>
-			<option disabled v-if="!panels" value="no panels"></option>
+		<option v-if="panels" v-for="panel in panels" :value="panel.panelId + '@' + panel.nodeID">{{ panel.panelId }} @
+			{{ panel.nodeID }}</option>
+		<option disabled v-if="!panels" value="no panels"></option>
 	</select>
 	<div class="row" v-if="renderComponent">
-		<!-- START CARDS --> 
+		<!-- START CARDS -->
 		<div class="col-sm-4 mb-3">
-    <div class="card h-100">
-        <div class="card-body">
-            <p class="card-text">
-                Spendable balance:<small style="float: right"></small>
-            </p>
-            <h2 class="card-title">{{ balance }} </h2>
-            <br />
-            <div style="padding: 0px; margin: 0px">
-                <a
-                    href="#"
-                    style="float: right; bottom: 0; margin-left: 5px"
-                    class="btn btn-outline-theme"
-                    >Top up</a
-                >
-            </div>
-        </div>
-        <div class="card-arrow">
-            <div class="card-arrow-top-left"></div>
-            <div class="card-arrow-top-right"></div>
-            <div class="card-arrow-bottom-left"></div>
-            <div class="card-arrow-bottom-right"></div>
-        </div>
-    </div>
-</div>
-<div class="col-sm-8 mb-3">
-    <div class="card h-100">
-        <div class="card-body"><table class="table dashtable table-borderless">
-  <thead>
-    <tr>
-      <th scope="col">Status</th>	
-      <th scope="col">Host</th>
-      <th scope="col">PanelId</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">
-		<div class="online"></div>
-	  </th>
-      <td>google.com</td>
-      <td>Hey</td>
-    </tr>
-    <tr>
-      <th scope="row"><div class="warning"></div></th>
-      <td>www.eitsmfe.be-19.info</td>
-      <td>hey</td>
-    </tr>
-    <tr>
-      <td scope="row"><div class="offline"></div></td>
-      <td>www.dienst.pw</td>
-	  <td >Hey</td>
- </tr>
+			<div class="card h-100">
+				<div class="card-body">
+					<p class="card-text">
+						Spendable balance:<small style="float: right"></small>
+					</p>
+					<h2 class="card-title">{{ balance }} </h2>
+					<br />
+					<div style="padding: 0px; margin: 0px">
+						<a href="#" style="float: right; bottom: 0; margin-left: 5px" class="btn btn-outline-theme">Top
+							up</a>
+					</div>
+				</div>
+				<div class="card-arrow">
+					<div class="card-arrow-top-left"></div>
+					<div class="card-arrow-top-right"></div>
+					<div class="card-arrow-bottom-left"></div>
+					<div class="card-arrow-bottom-right"></div>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-8 mb-3">
+			<div class="card h-100">
+				<div class="card-body">
+					<table class="table dashtable table-borderless" v-if="sitesWidget">
+						<thead>
+							<tr>
+								<th scope="col">Status</th>
+								<th scope="col">Host</th>
+								<th scope="col">PanelId</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr v-for="site in sitesWidget">
+								<th scope="row">
+									<div class="online" v-if="site.hostStatus == 'ONLINE'"></div>
+									<div class="warning" v-if="site.hostStatus == 'SSL_DOWN'"></div>
+									<div class="warning" v-if="site.hostStatus == 'PENDING_CHECK'"></div>
+									<div class="offline" v-if="site.hostStatus == 'DOMAIN_DOWN'"></div>
+									<div class="offline" v-if="site.hostStatus == 'SITE_DOWN'"></div>
+								</th>
+								<td>{{ site.domain }}</td>
+								<td>{{ site.panelId }}</td>
+							</tr>
+						</tbody>
+					</table>
+					<h5 v-else>No sites available</h5>
+				</div>
+				<div class="card-arrow">
+					<div class="card-arrow-top-left"></div>
+					<div class="card-arrow-top-right"></div>
+					<div class="card-arrow-bottom-left"></div>
+					<div class="card-arrow-bottom-right"></div>
+				</div>
+			</div>
+		</div>
 
-  </tbody>
-</table>
-        </div>
-        <div class="card-arrow">
-            <div class="card-arrow-top-left"></div>
-            <div class="card-arrow-top-right"></div>
-            <div class="card-arrow-bottom-left"></div>
-            <div class="card-arrow-bottom-right"></div>
-        </div>
-    </div>
-</div>
-
-<!-- END CARDS -->
-<!-- BEGIN stats -->
-<div style="flex-grow: 1"></div>
-<!-- END stats -->
+		<!-- END CARDS -->
+		<!-- BEGIN stats -->
+		<div style="flex-grow: 1"></div>
+		<!-- END stats -->
 
 		<!-- END-->
-		
+
 		<!-- BEGIN traffic-analytics -->
 		<div class="col-xl-6">
 			<card class="mb-3">
@@ -390,7 +402,7 @@ export default {
 							<div id="map"></div>
 						</div>
 					</div>
-					
+
 					<div class="row gx-4">
 						<div class="col-lg-6 mb-3 mb-lg-0">
 							<table class="w-100 small mb-0 text-truncate text-inverse text-opacity-60">
@@ -403,9 +415,9 @@ export default {
 									</tr>
 								</thead>
 								<tbody>
-									<tr v-if="stats && stats.locations" v-for="location in stats.locations.slice(0,5)">
+									<tr v-if="stats && stats.locations" v-for="location in stats.locations.slice(0, 5)">
 										<td>{{ location.Country ? location.Country : "Unknown" }}</td>
-										<td>{{ location.City ? location.City: "Unknown" }}</td>
+										<td>{{ location.City ? location.City : "Unknown" }}</td>
 										<td class="text-end">{{ location.Count }}</td>
 										<td class="text-end">{{ this.calcPercent(location) }}</td>
 									</tr>
@@ -420,15 +432,17 @@ export default {
 								<card-body class="py-2">
 									<div class="d-flex align-items-center">
 										<div class="w-70px">
-											<apexchart key="osChartKey" ref="osChartElem" :height="chartCfg.osChart.height" :options="chartCfg.osChart.options" :series="this.getOsData()"></apexchart>
+											<apexchart key="osChartKey" ref="osChartElem" :height="chartCfg.osChart.height"
+												:options="chartCfg.osChart.options" :series="this.getOsData()"></apexchart>
 										</div>
 										<div class="flex-1 ps-2">
 											<table class="w-100 small mb-0 text-inverse text-opacity-60">
 												<tbody>
-													<tr v-if="stats && stats.os" v-for="(os,index) in stats.os.slice(0,5)">
+													<tr v-if="stats && stats.os" v-for="(os, index) in stats.os.slice(0, 5)">
 														<td>
 															<div class="d-flex align-items-center">
-																<div class="w-6px h-6px rounded-pill me-2 bg-theme" :style="'opacity:0.'+(95-(20*index))"></div> 
+																<div class="w-6px h-6px rounded-pill me-2 bg-theme"
+																	:style="'opacity:0.' + (95 - (20 * index))"></div>
 																{{ os.OS ? os.OS : "Unknown" }}
 															</div>
 														</td>
@@ -449,7 +463,7 @@ export default {
 			</card>
 		</div>
 		<!-- END traffic-analytics -->
-			
+
 		<!-- BEGIN activity-log -->
 		<div class="col-xl-6">
 			<card class="mb-3">
@@ -463,31 +477,33 @@ export default {
 							<tbody>
 								<tr v-if="stats && stats.activity" v-for="item in stats.activity">
 									<td>
-										<i :class="'bi bi-'+item.icon"></i>
+										<i :class="'bi bi-' + item.icon"></i>
 										{{ item.content }}
 									</td>
 									<td><small>{{ timeAgo(item.time) }}</small></td>
 									<td>
-										<span class="badge d-block rounded-0 pt-5px w-70px" v-bind:class="{ 'bg-theme text-theme-900': item.isAlerted, 'bg-inverse bg-opacity-25': !item.isAlerted}" style="min-height: 18px">
+										<span class="badge d-block rounded-0 pt-5px w-70px"
+											v-bind:class="{ 'bg-theme text-theme-900': item.isAlerted, 'bg-inverse bg-opacity-25': !item.isAlerted }"
+											style="min-height: 18px">
 											{{ item.type.toUpperCase() }}
 										</span>
 									</td>
-									<td><a href="#" class="text-decoration-none text-inverse"><i class="bi bi-search"></i></a></td>
+									<td><a href="#" class="text-decoration-none text-inverse"><i
+												class="bi bi-search"></i></a></td>
 								</tr>
-								<tr v-else >
+								<tr v-else>
 									<td colspan="4">
 										No records found
 									</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</card-body>
-			</card>
-		</div>
-		<!-- END activity-log -->
-
-
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</card-body>
+		</card>
 	</div>
-	<!-- END row -->
-</template>
+	<!-- END activity-log -->
+
+
+</div>
+<!-- END row --></template>
