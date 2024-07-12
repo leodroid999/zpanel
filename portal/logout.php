205d90a254cec4efa3c0d9f0b9920c4c0c3ae1aa
leodroid999 <jackyacoub047@gmail.com>
2024-07-13 03:31:12 +0900

    remember me - logout problem fixed

--- portal/logout.php
+++ portal/logout.php
@@ -2,16 +2,30 @@
 include "cors.php";
 include 'lib/DB.php';
 
+use Library\DB as DB;
+
+$conn = DB::connect();
+
+// Check connection
+if (!$conn) {
+    error_log("Connection failed: " . mysqli_connect_error());
+}
+
 session_start();
 $userId = $_SESSION['userID'];
+$today = Date('Y-m-d H:i:s');
 
-$query = "UPDATE users SET remember_token='', remember_expires='' where userId='$userId'";
+$query = "UPDATE users SET remember_token='', remember_expires='$today' where userId='$userId'";
+
+
 if (mysqli_query($conn, $query)) {
+
     session_destroy();
+
     $SUCCESS = json_encode(
         array(
             'status' => "ok",
-            'message' => "Logout sucessfull"
+            'message' => "Logout sucessfull",
         )
     );
 } else
